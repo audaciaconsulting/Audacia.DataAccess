a7a68cd978f7676811d41e3f7239215703baeed8
Philip Rashleigh <philip.rashleigh@audacia.co.uk>
2018-02-26 11:59:21 +0000

    Basic auditing infrastructure

---
+++ src/Audacia.DataAccess.EntityFrameworkCore.Auditing/AuditDbCommandListener.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DiagnosticAdapter;
+
+namespace Audacia.DataAccess.EntityFrameworkCore.Auditing
+{
+    //https://weblogs.asp.net/ricardoperes/interception-in-entity-framework-core
+    public sealed class AuditDbCommandListener
+    {
+        private readonly ICollection<FireAndForgetDbCommandAuditor> _fireAndForgetAuditors =
+            new List<FireAndForgetDbCommandAuditor>();
+
+        private readonly ICollection<BlockingDbCommandAuditor> _blockingAuditors =
+            new List<BlockingDbCommandAuditor>();
+
+        public AuditDbCommandListener AddAuditor(FireAndForgetDbCommandAuditor auditor)
+        {
+            _fireAndForgetAuditors.Add(auditor);
+
+            return this;
+        }
+
+        public AuditDbCommandListener AddAuditor(BlockingDbCommandAuditor auditor)
+        {
+            _blockingAuditors.Add(auditor);
+
+            return this;
+        }
+
+        [DiagnosticName("Microsoft.EntityFrameworkCore.Database.Command.CommandExecuting")]
+        public void OnCommandExecuting(DbCommand command, DbCommandMethod executeMethod, Guid commandId,
+            Guid connectionId, bool async, DateTimeOffset startTime)
+        {
+            foreach (var fireAndForgetDbCommandAuditor in _fireAndForgetAuditors)
+            {
+                Task.Run(async () =>
+                    await fireAndForgetDbCommandAuditor.OnCommandExecutingAsync(command, executeMethod, commandId,
+                        connectionId, async, startTime));
+            }
+
+            foreach (var blockingDbCommandAuditor in _blockingAuditors)
+            {
+                blockingDbCommandAuditor.OnCommandExecuting(command, executeMethod, commandId, connectionId, async,
+                    startTime);
+            }
+        }
+
+        [DiagnosticName("Microsoft.EntityFrameworkCore.Database.Command.CommandExecuted")]
+        public void OnCommandExecuted(object result, bool async)
+        {
+            foreach (var fireAndForgetDbCommandAuditor in _fireAndForgetAuditors)
+            {
+                Task.Run(async () =>
+                    await fireAndForgetDbCommandAuditor.OnCommandExecutedAsync(result, async));
+            }
+
+            foreach (var blockingDbCommandAuditor in _blockingAuditors)
+            {
+                blockingDbCommandAuditor.OnCommandExecuted(result, async);
+            }
+        }
+    }
+}
---
+++ src/Audacia.DataAccess.EntityFrameworkCore.Auditing/DbContextExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Audacia.DataAccess.EntityFrameworkCore.Auditing
+{
+    public static class DbContextExtensions
+    {
+        private static readonly ConditionalWeakTable<DbContext, AuditDbCommandListener> AuditDbCommandListenerLookup =
+            new ConditionalWeakTable<DbContext, AuditDbCommandListener>();
+
+        public static void AddAuditor(this DbContext dbContext, BlockingDbCommandAuditor auditor)
+        {
+            auditor.RegisterContext(dbContext);
+
+            dbContext.InitOrGetAuditDbCommandListener().AddAuditor(auditor);
+        }
+
+        public static void AddAuditor(this DbContext dbContext, FireAndForgetDbCommandAuditor auditor)
+        {
+            auditor.RegisterContext(dbContext);
+
+            dbContext.InitOrGetAuditDbCommandListener().AddAuditor(auditor);
+        }
+
+        private static AuditDbCommandListener InitOrGetAuditDbCommandListener(this DbContext dbContext)
+        {
+            if (!AuditDbCommandListenerLookup.TryGetValue(dbContext, out var auditDbCommandListener))
+            {
+                auditDbCommandListener = new AuditDbCommandListener();
+
+                var diagnosticListener = dbContext.GetService<DiagnosticSource>() as DiagnosticListener;
+
+                diagnosticListener.SubscribeWithAdapter(auditDbCommandListener);
+            }
+
+            return auditDbCommandListener;
+        }
+    }
+}
