c93b7dcd50a07cfd7a4ec1d552d6afd2d1b72fc7
Owen Lacey <owen.lacey@audacia.co.uk>
2021-11-10 12:08:15 +0000

    - Update Audacia.Core version - Add ToPageAsync extensions with tests

---
+++ src/Audacia.DataAccess.EntityFrameworkCore/Extensions/QueryableExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Audacia.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Audacia.DataAccess.EntityFrameworkCore.Extensions
+{
+    /// <summary>
+    /// Extensions for the <see cref="IQueryable{T}"/> type
+    /// </summary>
+    public static class QueryableExtensions
+    {
+        /// <summary>
+        /// Return a page of information based on the provided <paramref name="pagingRequest"/>.
+        /// </summary>
+        /// <param name="query">Our query against the result set.</param>
+        /// <param name="pagingRequest">Contains information required for paging & sorting.</param>
+        /// <param name="cancellationToken">For cancelling asynchronous tasks, passed into EF enumeration methods.</param>
+        /// <typeparam name="T">The type of each row of data on the page.</typeparam>
+        /// <returns>A <see cref="Page{T}"/> of results.</returns>
+        public static async Task<IPage<T>> ToPageAsync<T>(this IQueryable<T> query, PagingRequest pagingRequest,
+            CancellationToken cancellationToken = default)
+        {
+            var totalRecords = await query.CountAsync(cancellationToken);
+
+            var specification = new PagingSpecification<T>(query)
+                .ConfigurePaging(pagingRequest)
+                .UsePaging();
+
+            var data = await specification.Query.ToListAsync(cancellationToken);
+            var totalPages = specification.GetTotalPages(totalRecords);
+
+            return new Page<T>(data, totalPages, totalRecords);
+        }
+
+        /// <summary>
+        /// Return a page of information, with sorting applied, based on the provided <paramref name="pagingRequest"/>.
+        /// </summary>
+        /// <param name="query">Our query against the result set.</param>
+        /// <param name="pagingRequest">Contains information required for paging & sorting.</param>
+        /// <param name="cancellationToken">For cancelling asynchronous tasks, passed into EF enumeration methods.</param>
+        /// <typeparam name="T">The type of each row of data on the page.</typeparam>
+        /// <returns>A <see cref="Page{T}"/> of results.</returns>
+        public static async Task<IPage<T>> ToPageAsync<T>(this IQueryable<T> query, SortablePagingRequest pagingRequest,
+            CancellationToken cancellationToken = default)
+        {
+            var totalRecords = await query.CountAsync(cancellationToken);
+
+            var specification = new PagingSpecification<T>(query)
+                .ConfigurePaging(pagingRequest)
+                .ConfigureSorting(pagingRequest)
+                .UseSorting()
+                .UsePaging();
+
+            var data = await specification.Query.ToListAsync(cancellationToken);
+            var totalPages = specification.GetTotalPages(totalRecords);
+
+            return new Page<T>(data, totalPages, totalRecords);
+        }
+    }
+}
